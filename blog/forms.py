066c4dc8c2901aaf65304b0ad7594c49ae2f8392
Ivailo Iliqnov Iliev <ivailoiliev89@gmail.com>
2026-01-22 16:24:04 +0200

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,16 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['body']
         widgets = {
-            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Напиши коментар...'})
+            'body': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a comment...'})
         }
         labels = {'body': ''}
 
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'image', 'category', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(attrs={'class': 'custom-tag-list'}),
+            'content': forms.Textarea(attrs={'rows': 8, 'class': 'form-control'}),
+        }
