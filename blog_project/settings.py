9c3880627b34f953b52e101ff53e72e82291779d
Ivailo Iliqnov Iliev <ivailoiliev89@gmail.com>
2026-01-22 16:05:40 +0200

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -79,9 +79,12 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'blog:post_list'
 LOGOUT_REDIRECT_URL = 'blog:post_list'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
